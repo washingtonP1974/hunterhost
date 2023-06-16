879f521afdba19a30d2ea551f9bde17d88f7e39c
morgan/bin/bash <55928887+washingtonP1974@users.noreply.github.com>
2023-06-16 13:39:41 -0300

    Update hunterhost.py

--- hunterhost.py
+++ hunterhost.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # coding: utf-8
-#
-# Layer 2 network neighbourhood discovery tool
-# written by Benedikt Waldvogel (mail at bwaldvogel.de)
 
 from __future__ import absolute_import, division, print_function
 import os
